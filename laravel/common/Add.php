71ff0f36f1541d0b51625204f584a78193d7ed00
PagerNotFound <leafnotfound@qq.com>
2015-06-07 12:18:54 +0800

    include our code libriary
    Don't forget to run `composer dumpautoload`.

---
+++ laravel/common/Add.php
@@ -0,0 +1,20 @@
+<?php namespace Common;
+
+/**
+ * Class Add
+ * @package Common
+ */
+class Add
+{
+
+    /**
+     * @param $foo
+     * @param $bar
+     * @return mixed
+     */
+    static function execute($foo, $bar)
+    {
+        return $foo + $bar;
+    }
+
+}
